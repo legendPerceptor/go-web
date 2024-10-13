f60c19523853da5ba19c75a8ee6342ac53b955b9
Yuanjian Liu <achievinglegend@gmail.com>
2024-10-12 21:12:22 -0500

    the comps project, with tailwindcss

---
+++ web-frontend/comps/src/App.js
@@ -0,0 +1,38 @@
+import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
+import Button from './Button';
+
+
+function App() {
+    return <div>
+        <div>
+            <Button primary rounded outline
+            className="mb-5">
+                <GoBell/>
+                Click Me!
+            </Button>
+        </div>
+        <div>
+            <Button secondary>
+                <GoCloud/>
+                Hi there!
+            </Button>
+        </div>
+        <div>
+            <Button secondary outline>
+                <GoDatabase/>
+                Good!
+            </Button>
+        </div>
+        <div>
+            <Button success>Success!</Button>
+        </div>
+        <div>
+            <Button danger rounded>False!</Button>
+        </div>
+        <div>
+            <Button warning>Warning!</Button>
+        </div>
+    </div>;
+}
+
+export default App;
