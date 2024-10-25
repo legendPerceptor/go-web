ad7559b4544274d922dbd786d1710ec4aebda611
Yuanjian Liu <yuanjian@uchicago.edu>
2024-10-25 15:10:05 -0500

    add navigation context and popstate event listener

--- web-frontend/comps/src/App.tsx
+++ web-frontend/comps/src/App.tsx
@@ -1,14 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
 function App() {
-    const [selection, setSelection] = useState(null);
-    const options = [
-        {label: "Red", value: "red"},
-        {label: "Green", value: "green"},
-        {label: "Blue", value: "blue"}
-    ]
-
-    return <Dropdown options={options} value={selection} onChange={setSelection}/>;
+   return <div></div>
 }
 
 export default App;
---
+++ web-frontend/comps/src/pages/DropdownPage.js
@@ -0,0 +1,14 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+function DropdownPage() {
+    const [selection, setSelection] = useState(null);
+    const options = [
+        {label: "Red", value: "red"},
+        {label: "Green", value: "green"},
+        {label: "Blue", value: "blue"}
+    ]
+
+    return <Dropdown options={options} value={selection} onChange={setSelection}/>;
+}
+
+export default DropdownPage;
