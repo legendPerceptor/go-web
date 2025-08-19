19d2b61521dabf9150fa8e1627c48763ef4e4b7a
Yuanjian Liu <achievinglegend@gmail.com>
2025-08-19 23:40:14 +0800

    finish chapter 1, remove executables

---
+++ go-lang-basics/ch1/7_webserver/server1.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand/v2"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+var palette = []color.Color{color.Black, color.RGBA{0, 0xFF, 0, 0xFF},
+	color.RGBA{0xFF, 0, 0, 0xFF}, color.RGBA{0, 0, 0xFF, 0xFF} }
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		// cycles = 10 // number of complete x oscillator revolutions
+		res = 0.001 // angular resolution
+		size = 100 // image canvas covers [-size..+size]
+		nframes = 64 // number of animation frames
+		delay = 8 // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i:=0; i<nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t:=0.0; t<float64(cycles)*2*math.Pi; t+=res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			// colorIndex := uint8(rand.Intn(len(palette) - 1)) + 1
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), 1)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request) {
+		cycles, err := strconv.Atoi(r.URL.Query().Get("cycles"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot convert the cycles variable to int\n")
+			fmt.Fprintf(w, "Cannot convert the cycles variable to int\n")
+		}
+		lissajous(w, cycles)
+	})
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+        mu.Lock()
+        count++
+        mu.Unlock()
+    }
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
